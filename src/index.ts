e718e94e80f82e9baccd979eed9daf4085d1221a
Muhammad-Talha <muhammad.talha.776932@gmail.com>
2023-04-23 11:20:25 +0500

    added the comment and middleware.

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,26 @@
-import express from 'express';
+import * as dotenv from 'dotenv';
+import express, { Request, Response, Express } from 'express';
+import cors from 'cors'
 
-const app = express();
+// * Config the .env file here
+dotenv.config();
 
-app.get('/', (req, res) => {
-  res.send('Hello, world!');
+// * create the instence of Express 
+const app: Express = express();
+
+// * setup the middleware 
+
+app.use(cors());
+app.use(express.json({ limit: '50mb' }));
+
+
+// * handle the default route get request and response
+app.get('/', (req: Request, res: Response) => {
+    res.status(200).json({ message: "Hello from DALL.E" });
 });
 
+
+// * Listen the server at localhost:3000
 app.listen(3000, () => {
-  console.log('Server listening on port 3000');
+    console.log('Server listening on port 3000');
 });
